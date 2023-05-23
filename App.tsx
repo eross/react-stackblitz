9f42af8d72d298f6befa9b4ff98b430270a5ade3
eross <eross@users.noreply.github.com>
2023-05-23 16:35:12 -0700

    Eliminate the smiley face.

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello Eric!</h1>
-      <p>Start editing to see some magic happen here :)</p>
+      <p>Start editing to see some magic happen here.</p>
     </div>
   );
 }
