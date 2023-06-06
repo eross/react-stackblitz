bcc61b5abb878fbcdac8452a21c746c5363bffb5
eross <eross@users.noreply.github.com>
2023-06-06 12:31:50 -0700

    Add ExpenseItem

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import './css/style.css';
-import MyFunc from './components/MyFunc';
+import ExpenseItem from './components/ExpenseItem';
 
 export default function App() {
   return (
     <div>
       <h1>Hello Eric!</h1>
       <p>Start editing to see some magic happen here.</p>
-      <MyFunc></MyFunc>
+      <ExpenseItem />
     </div>
   );
 }
