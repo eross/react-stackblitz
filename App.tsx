ad397deb30b73bf57520f51af3fdb79ab06119b2
eross <eross@users.noreply.github.com>
2023-06-06 10:45:08 -0700

    Add submodule MyFunc

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import './style.css';
+import MyFunc from './MyFunc.tsx';
 
 export default function App() {
   return (
     <div>
       <h1>Hello Eric!</h1>
       <p>Start editing to see some magic happen here.</p>
+      <MyFunc></MyFunc>
     </div>
   );
 }
---
+++ MyFunc.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './style.css';
+
+export default function MyFunc() {
+  return (
+    <div>
+      <h2>How about a list from a module</h2>
+      <ul>
+        <li>First item.</li>
+        <li>Second Item.</li>
+      </ul>
+    </div>
+  );
+}
