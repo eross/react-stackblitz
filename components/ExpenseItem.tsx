b6f6040dc1d8d6cd65f55d9e841720a3dd86e617
eross <eross@users.noreply.github.com>
2023-06-06 19:45:55 -0700

    Add CSS for ExpenseItem

--- components/ExpenseItem.tsx
+++ components/ExpenseItem.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import './ExpenseItem.css';
 
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28, 2021</div>
+      <div className="expense-item__description"></div>
       <h2>Car Insurance</h2>
-      <div>294.67</div>
+      <div className="expense-item__price">294.67</div>
     </div>
   );
 }
