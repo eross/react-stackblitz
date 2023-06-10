9aa19c9096dec5b24803a9e1a2e4bc1c243cc972
eross <eross@users.noreply.github.com>
2023-06-09 17:55:59 -0700

    Add event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,6 +10,13 @@ function ExpenseItem(props) {
       <div className="expense-item__description"></div>
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
+      <button
+        onClick={() => {
+          console.log('Clicked!');
+        }}
+      >
+        Change Title
+      </button>
     </Card>
   );
 }
