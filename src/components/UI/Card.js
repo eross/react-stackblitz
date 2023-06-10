39ace1728fdccd089530e401bdee1d5fe1603b58
eross <eross@users.noreply.github.com>
2023-06-09 17:48:10 -0700

    merge Card changes

---
+++ src/components/UI/Card.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './Card.css';
+function Card(props) {
+  const classes = 'card ' + props.className;
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
