a3b1c270812b25d49b5790467a85cdbe237ffbfa
eross <eross@users.noreply.github.com>
2023-06-13 20:10:28 -0700

    NewExpense form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/style.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 export default function App() {
   const expenses = [
@@ -26,7 +27,7 @@ export default function App() {
   ];
   return (
     <div>
-      <h2>Let's get started</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
