c7e825fbfed8f28004d97c2504894ca5c8c2039e
eross <eross@users.noreply.github.com>
2023-06-14 11:29:18 -0700

    use single state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,19 +2,35 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState(0.0);
-  const [enteredDate, setEnteredDate] = useState('01/01/2019');
+  //  const [enteredTitle, setEnteredTitle] = useState('');
+  //  const [enteredAmount, setEnteredAmount] = useState(0.0);
+  //  const [enteredDate, setEnteredDate] = useState('01/01/2019');
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   };
 
   const dateChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
+
     setEnteredDate(event.target.value);
   };
 
