6befb909bded707200025ab1fbb49f4c304b0dc2
eross <eross@users.noreply.github.com>
2023-06-17 14:09:59 -0700

    used state title instead of props.title.  Caused title to not be updated.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -11,14 +11,12 @@ function ExpenseItem(props) {
     var t = nextTitle;
     setNextTitle(title);
     setTitle(t);
-    console.log(title);
-    console.log(nextTitle);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description"></div>
-      <h2>{title}</h2>
+      <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
